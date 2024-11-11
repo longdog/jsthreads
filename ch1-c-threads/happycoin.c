82c08998e982a22297bbc7c745ba08d5e1bf6327
longdog <linda@longdog.us>
2024-11-11 16:41:21 +0400

    add c example

--- ch1-c-threads/happycoin.c
+++ ch1-c-threads/happycoin.c
@@ -5,6 +5,46 @@
 #include <time.h>
 
 
+uint64_t random64 (uint32_t * seed){
+  uint64_t result;
+  uint8_t * result8 = (uint8_t *)&result;
+  for (size_t i = 0; i < sizeof(result); i++){
+    result8[i] = rand_r(seed);
+  }
+  return result;
+}
+
+uint64_t sum_digits_squared (uint64_t num){
+  uint64_t total = 0;
+  while (num > 0){
+    uint64_t num_mode_base = num % 10;
+    total += num_mode_base * num_mode_base;
+    num = num / 10;
+  }
+  return total;
+}
+
+bool is_happy (uint64_t num){
+  while (num != 1 && num != 4){
+    num = sum_digits_squared(num);
+  }
+  return num == 1;
+}
+
+bool is_happycoin (uint64_t num){
+  return is_happy(num) && num % 1000 == 0;
+}
+
 int main(){
-  printf ("hello");
+  uint32_t seed = time(NULL);
+  int count = 0;
+  for(int i = 1; i < 10000000; i++){
+    uint64_t random_num = random64(&seed);
+    if(is_happycoin(random_num)){
+      printf("%" PRIu64 " ", random_num);
+      count++;
+    }
+  }
+  printf("\ncount %d\n", count);
+  return 0;
 }
