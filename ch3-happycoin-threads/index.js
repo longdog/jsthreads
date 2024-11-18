241310bf4736095fa94c43bcac172a5dde032196
longdog <linda@longdog.us>
2024-11-18 13:32:50 +0400

    add happycoin multithread

---
+++ ch3-happycoin-threads/index.js
@@ -0,0 +1,61 @@
+import { Worker,
+  isMainThread,
+  parentPort
+} from "node:worker_threads"
+
+import crypto from "node:crypto"
+
+const big64arr = new BigUint64Array(1);
+function random64() {
+  crypto.randomFillSync(big64arr);
+  return big64arr[0];
+}
+
+function sumDigitsSquared(num) {
+  let total = 0n;
+  while (num > 0) {
+    const numModBase = num % 10n;
+    total += numModBase ** 2n;
+    num = num / 10n;
+  }
+  return total;
+}
+
+function isHappy(num) {
+  while (num != 1n && num != 4n) {
+    num = sumDigitsSquared(num);
+  }
+  return num === 1n;
+}
+
+function isHappycoin(num) {
+  return isHappy(num) && num % 10000n === 0n;
+}
+
+const THREAD_COUNT = 16;
+
+if (isMainThread) {
+  let inFlight = THREAD_COUNT;
+  let count = 0;
+  for (let i = 0; i < THREAD_COUNT; i++) {
+    const worker = new Worker(import.meta.filename);
+    worker.on('message', msg => {
+      if (msg === 'done') {
+        if (--inFlight === 0) {
+          process.stdout.write('\ncount ' + count + '\n');
+        }
+      } else if (typeof msg === 'bigint') {
+        process.stdout.write(msg.toString() + ' ');
+        count++;
+      }
+    })
+  }
+} else {
+  for (let i = 1; i < 10_000_000/THREAD_COUNT; i++) {
+    const randomNum = random64();
+    if (isHappycoin(randomNum)) {
+      parentPort.postMessage(randomNum);
+    }
+  }
+  parentPort.postMessage('done');
+}
